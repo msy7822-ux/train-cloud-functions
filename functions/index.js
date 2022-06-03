1e1dc68f4ac89849d68337a97879543120ca9f66
msy7822 <msy780022@gmail.com>
2022-06-03 09:12:31 +0900

    isbnから書籍を取得する関数を実装

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,26 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+// firestoreからbooksドキュメントを取得するエンドポイント
+exports.fetchBookInfo = functions.https.onRequest((request, response) => {
+  // fetchBookInfo関数では、GETリクエスト以外のリクエスト全てに対してエラーを返す
+  if (request.method !== 'GET') response.status(400).send("リクエストタイプが不正です。");
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  // クエリとしてisbn番号を受け取る
+  const isbn = request.query.isbn;
+
+  // クエリ文字列が渡されなかったらエラーを返す
+  if (!isbn) response.status(400).send('クエリが不正です。');
+
+  const db = admin.firestore();
+  db.collection('books').doc(isbn).get()
+    .then(res => {
+      const json = JSON.stringify(res.data(), null, '\t');
+      response.send(json);
+      return null;
+    }).catch(err => {
+      console.log(err);
+      response.status(500).send(err);
+    });
 });
