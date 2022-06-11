33892087d2e644a32f373c914bb99f1979c8caf0
msy7822 <msy780022@gmail.com>
2022-06-11 13:49:07 +0900

    不要な標準出力を削除

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,6 @@ const db = admin.firestore();
 // user認証が完了してユーザーが追加された時に発火する処理
 exports.sendWelcomeEmail = functions.auth.user().onCreate(async (user) => {
   const {email} = user;
-  console.log(user);
   console.info(`登録ユーザーのE-Mail:${email}`);
 
   // メールを送信する処理
